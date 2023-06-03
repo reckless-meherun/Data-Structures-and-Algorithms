aa50dac9cb618cd377e7523923b9fe286796605b
reckless-meherun <meherun2001@gmail.com>
2023-06-03 18:09:00 +0600

    adjacent matrix graph updated

--- DSA/Data Structures/Graph/GraphAdjMat.cpp
+++ DSA/Data Structures/Graph/GraphAdjMat.cpp
@@ -19,7 +19,7 @@ public:
 
         /** have to locate memory for the container arrays first, then we can declare some size of array for each array */
         adjMat = new int *[this->vertices + 1];        
-        for (int i = 0; i <= vertices; i++)
+        for (int i = 1; i <= vertices; i++)
         {
             adjMat[i] = new int[vertices + 1];
         }
@@ -31,7 +31,7 @@ public:
 
     void defineGraph()
     {
-        for (int i = 0; i < edges; i++)
+        for (int i = 1; i <= edges; i++)
         {
             int u, v;
             cin >> u >> v;
@@ -52,11 +52,36 @@ public:
         adjMat[v][u] = false;
     }
 
+    bool isConnected(int u, int v)
+    {
+        return adjMat[u][v] or adjMat[v][u];
+    }
+
+    int outDegree(int u)
+    {
+        int outD = 0;
+        for(int i=1; i<=vertices; i++)
+        {
+            outD += adjMat[u][i];
+        }
+        return outD;
+    }
+
+    int inDegree(int u)
+    {
+        int inD = 0;
+        for(int i=1; i<=vertices; i++)
+        {
+            inD += adjMat[i][u];
+        }
+        return inD;
+    }
+
     void printGraph()
     {
-        for (int i = 0; i < vertices; i++)
+        for (int i = 1; i <= vertices; i++)
         {
-            for (int j = 0; j < vertices; j++)
+            for (int j = 1; j <= vertices; j++)
                 cout << adjMat[i][j] << " ";
             cout << endl;
         }
@@ -65,11 +90,14 @@ public:
 
 int main()
 {
-    freopen("input.in", "r", stdin);
-    freopen("output.in", "w", stdout);
+    // freopen("input.in", "r", stdin);
+    // freopen("output.in", "w", stdout);
 
-    graph g(4, 4, false);
+    graph g(5, 6, true); //starts from 1
     g.defineGraph();
     g.printGraph();
+    cout<<g.isConnected(1,3)<<endl;
+    cout<<g.inDegree(2)<<endl;
+    cout<<g.outDegree(2)<<endl;
     return 0;
 }
